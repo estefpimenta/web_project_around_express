06efd3dfa72cf83a12037d01d0517c3cd7c73f39
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-05-17 10:15:53 -0300

    update confogs and app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.get('/', (req, res) => {
+  res.send('Servidor Express rodando na porta 3000');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
